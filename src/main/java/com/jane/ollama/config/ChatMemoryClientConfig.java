9d80bb3a345cee5637785da7cb99b4173e2b52e4
Ritu Bafna <ritujane78@gmail.com>
2026-01-28 13:05:13 +0545

    implement RAG also by storing the chunks of a pdf file in vector database.

--- src/main/java/com/jane/ollama/config/ChatMemoryClientConfig.java
+++ src/main/java/com/jane/ollama/config/ChatMemoryClientConfig.java
@@ -9,6 +9,9 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,11 +25,20 @@ public class ChatMemoryClientConfig {
                 .chatMemoryRepository(repository).build();
     }
     @Bean("chatMemoryChatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, RetrievalAugmentationAdvisor retrievalAugmentationAdvisor) {
         Advisor loggerAdvisor = new SimpleLoggerAdvisor();
         Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
         return chatClientBuilder
-                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor, retrievalAugmentationAdvisor))
                 .build();
+    }
+    @Bean
+    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore){
+
+    return RetrievalAugmentationAdvisor.builder().documentRetriever(
+            VectorStoreDocumentRetriever.builder()
+                    .vectorStore(vectorStore)
+                    .topK(3).similarityThreshold(0.5).build()
+    ).build();
     }
 }
